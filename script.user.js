f7f5ec9f366d7b36260076ca9709a91c22519fba
从删库到跑路的自我修养 <zjc877@outlook.com>
2019-06-23 15:59:31 +0800

    Create script.user.js

---
+++ script.user.js
@@ -0,0 +1,86 @@
+// ==UserScript==
+// @name         潭州课堂助手
+// @namespace    http://tampermonkey.net/
+// @version      1.01
+// @description  去除潭州课堂播轮播广告，头像下增加我的作业菜单，昵称完整展示。
+// @author       QQ：619877197   Email：zjc877@outlook.com
+// @match        *://*.shiguangkey.com/*
+// @require      https://cdn.bootcss.com/jquery/3.4.1/jquery.min.js
+// @require      https://cdn.bootcss.com/jquery-cookie/1.4.1/jquery.cookie.min.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    window.onload = function () {
+
+        // 移除轮播广告
+        $("div[class='_1-j4s']").remove();
+
+        // 直接展示课程目录
+        var tabs = $("div[role='tab']");
+        $.each(tabs, function (i, item) {
+            if ($(item).text() === "课程目录") {
+                $(item).click();
+                return false;
+            }
+        });
+
+        // 头像下面添加我的作业菜单
+        var avatarlis = $("div[class='memberHome___ReVBw']").length > 0 ? $("div[class='memberHome___ReVBw']").find("ul").find("li") : $("div[class='_2v85p']").find("ul").find("li");
+        $.each(avatarlis, function(i, item) {
+            if ($(item).find("a").text() === "我的课表") {
+                var homeworkdom = $("<li><a href='/i/homework'>我的作业</a></li>");
+                $(item).after(homeworkdom);
+            }
+        })
+
+        // 昵称完整展示
+        var nickdom = $("span[class='avatarNick___3PfCp userNickname']").length > 0 ? $("span[class='avatarNick___3PfCp userNickname']") : $("span[class='_3Au-A userNickname']")
+        nickdom.css({
+            "margin-right": "30px",
+            "overflow": "visible"
+        })
+
+
+        //
+
+        var homeworktabs = $("div[role='tab']")
+        $.each(homeworktabs, function(i, item) {
+            if (RegExp(/已完成/).exec($(item).find("p").text())) {
+
+                //var completednum = $($("tbody[class='rc-table-tbody']")[1]).find("tr").length;
+                document.cookie = document.cookie;
+                $.ajax({
+                    type: "GET",
+                    async: false,
+                    //crossDomain: true,
+                    xhrFields: {
+                        withCredentials: true
+                    },
+                    headers: {
+
+                        "token": $.cookie("token"),
+                        "terminaltype": 4
+                    },
+                    url: "https://www.shiguangkey.com/i/homework/chapterHomework?classId=6935&activeKey=1&pageIndex=1&pageSize=10&terminalType=4",
+                    //dataType: "text",
+                    success: function (res) {
+                        console.log(res);
+                        var completed = $("<span style='color: green;'>（" + res.data.totalItem + "）</span>");
+                        $(item).find("p").append(completed);
+                    },
+                    error: function () {}
+                });
+
+            }
+
+        })
+
+
+
+    };
+
+
+})();
