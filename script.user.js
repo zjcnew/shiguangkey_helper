102e56ce3fee8f039f9d81f6325196d3af0313a0
从删库到跑路的自我修养 <zjc877@outlook.com>
2019-06-26 01:21:03 +0800

    最近在学按钮点击恢复状态，开课提醒功能的实现；元素点击事件逻辑小版本重构

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         潭州课堂助手
 // @namespace    http://tampermonkey.net/
-// @version      1.04
+// @version      1.05
 // @description  去除潭州课堂播轮播广告，头像下增加我的作业菜单，昵称完整展示，作业数量醒目展示。
 // @author       QQ：619877197   Email：zjc877@outlook.com
 // @match        *://*.shiguangkey.com/*
@@ -50,46 +50,12 @@
                 $.each(homeworktabs, function(i, item) {
                     var labelp = $(item).find("p");
                     if (RegExp(/待完成/).exec(labelp.text())) {
-                        if (labelp.find("span[class='shiguangkey_helper']").length == 0) {
-                            $.ajax({
-                                type: "GET",
-                                async: false,
-                                cache: false,
-                                headers: {
-                                    "token": $.cookie.get("token"),
-                                    "terminaltype": 4
-                                },
-                                cookie: document.cookie,
-                                url: "https://www.shiguangkey.com/api/student/homework/getUnCompletedList?classId=" + $.getUrlParam("classId") + "&activeKey=1&pageIndex=1&pageSize=10&terminalType=4",
-                                dataType: "json",
-                                success: function (res) {
-                                    var uncompleted = $("<span style='color: red;' class='shiguangkey_helper'>（" + res.data.totalItem + "）</span>");
-                                    labelp.find("span[style='color: red;']").remove();
-                                    labelp.append(uncompleted);
-                                },
-                                error: function () {}
-                            });
+                        if (labelp.find("span[class='shiguangkeyHelper']").length == 0) {
+                            shiguangkeyHelper.sethomeworknum("uncompleted", labelp);
                         }
                     } else if (RegExp(/已完成/).exec(labelp.text())) {
-                        if (labelp.find("span[class='shiguangkey_helper']").length == 0) {
-                            $.ajax({
-                                type: "GET",
-                                async: false,
-                                cache: false,
-                                headers: {
-                                    "token": $.cookie.get("token"),
-                                    "terminaltype": 4
-                                },
-                                cookie: document.cookie,
-                                url: "https://www.shiguangkey.com/api/student/homework/getCompletedList?classId=" + $.getUrlParam("classId") + "&activeKey=1&pageIndex=1&pageSize=10&terminalType=4",
-                                dataType: "json",
-                                success: function (res) {
-                                    var completed = $("<span style='color: green;' class='shiguangkey_helper'>（" + res.data.totalItem + "）</span>");
-                                    labelp.find("span[style='color: green;']").remove();
-                                    labelp.append(completed);
-                                },
-                                error: function () {}
-                            });
+                        if (labelp.find("span[class='shiguangkeyHelper']").length == 0) {
+                            shiguangkeyHelper.sethomeworknum("completed", labelp);
                         }
                     }
                 })
@@ -120,49 +86,133 @@
 
             // 我的课程表增加最近在学
             (function() {
-                var lis = $("div[class='_2HiiN']").find("ul").find("li");
-                for (var i=0; i<lis.length; i++) {
-                    if ($(lis[i]).find("a[class='shiguangkey_helper']").length == 1) {
-                        return;
-                    }
+                if ($("a[onclick='shiguangkeyHelper.lastlearnselect()']").length == 1) {
+                    return;
                 }
-                var lastlearn = $("<li class><a href='javascript:;' style='color: blue; font-weight: bold' class='shiguangkey_helper' onclick = '$(this).parent().addClass(\"_2Aysu\");'>最近在学</a></li>");
+                var lastlearn = $("<li class><a href='javascript:;' style='color: blue; font-weight: bold' onclick='shiguangkeyHelper.lastlearnselect()'>最近在学</a></li>");
                 $("div[class='_2HiiN']").find("ul").append(lastlearn);
-                lastlearn.click(function() {
-                    $.ajax({
-                        type: "POST",
-                        async: false,
-                        cache: false,
-                        headers: {"token": $.cookie.get("token")},
-                        cookie: document.cookie,
-                        url: "https://www.shiguangkey.com/api/course/queryByStudent",
-                        data: {"pageIndex": 1, "pageSize":10, "courseType": 5310, "terminalType":4},
-                        dataType: "json",
-                        success: function (res) {
-                            var courses = res.data.courses;
-                            for (var i=0; i<courses.length; i++) {
-                                var course = courses[i];
-                                if (course.liveChapterStartTime || course.startTimeMills || course.startTime) {
-                                    var courseHtml = '<div class="TlhQy"><a href="/course/{id}" target="_blank" rel="noopener noreferrer" class="_1TOYf"><img src="https://res.shiguangkey.com/{cover}?x-oss-process=image/format,webp" alt="封面"><span class="_27EfR _2VfA1">VIP</span></a><div class="_3f44N"><a href="/course/{id}" target="_blank" rel="noopener noreferrer" class="_1gBLk">{title}</a><div><div class="_25oe4">开课时间：<b>{t}</b></div><div class="_25oe4">所在班期：<b>{period}期</b></div></div><div class="_19ULe"><div class="_2_52z" href="javascript:;">联系老师<ul><div class="_1Zcg9"><div class="_2u4ro"><li><a href="tencent://message/?uin=3003302254&amp;Site=qq&amp;Menu=yes" target="_blank" rel="noopener noreferrer">在线查考勤木子</a></li><li><a href="tencent://message/?uin=2917322537&amp;Site=qq&amp;Menu=yes" target="_blank" rel="noopener noreferrer">05179-Tuple</a></li><li><a href="tencent://message/?uin=3002727582&amp;Site=qq&amp;Menu=yes" target="_blank" rel="noopener noreferrer">塔卡-05028</a></li><li><a href="tencent://message/?uin=&amp;Site=qq&amp;Menu=yes" target="_blank" rel="noopener noreferrer">Sky老师</a></li><li><a href="tencent://message/?uin=2446867994&amp;Site=qq&amp;Menu=yes" target="_blank" rel="noopener noreferrer">心蓝06258</a></li><li><a href="tencent://message/?uin=3002751317&amp;Site=qq&amp;Menu=yes" target="_blank" rel="noopener noreferrer">缇子05015</a></li><li><a href="tencent://message/?uin=3003806927&amp;Site=qq&amp;Menu=yes" target="_blank" rel="noopener noreferrer">忘仙_03179</a></li><li><a href="tencent://message/?uin=3002795033&amp;Site=qq&amp;Menu=yes" target="_blank" rel="noopener noreferrer">木子</a></li><li><a href="tencent://message/?uin=&amp;Site=qq&amp;Menu=yes" target="_blank" rel="noopener noreferrer">小拓</a></li><li><a href="tencent://message/?uin=3003302254&amp;Site=qq&amp;Menu=yes" target="_blank" rel="noopener noreferrer">南小帝</a></li><li><a href="tencent://message/?uin=&amp;Site=qq&amp;Menu=yes" target="_blank" rel="noopener noreferrer">Youkou</a></li><li><a href="tencent://message/?uin=879735038&amp;Site=qq&amp;Menu=yes" target="_blank" rel="noopener noreferrer">蓝羽</a></li></div></div></ul></div></div></div><div class="zBr3m"><span class="_3dqyA"><span class="WM317"></span></span><span>开课提醒已关闭</span></div><div style="display: flex; flex-direction: column; float: right;"><button class="_3xDIx" onclick="(function() {location.href = \'https://www.shiguangkey.com/live/{id}/{classId}\'})();">开始学习</button></div></div>'
-                                    course.t = $.timeFormat((new Date(course.liveChapterStartTime)), "yyyy-MM-dd hh:mm")
-                                    courseHtml = $.stringFormat(courseHtml, course)
-                                    var courses_div = $("div[class='_2GxAN']").find("div[class='_2u4ro']");
-                                    $(courses_div.find("div[class='TlhQy']")).remove();
-                                    courses_div.append($(courseHtml));
-                                }
-                                $("div[class='_3vI_T']").remove();
-                            }
-                        },
-                        error: function () {}
-                    })
-                });
             })();
-
 
         }, 500)
 
-
     };
+
+    window.shiguangkeyHelper = {
+        sethomeworknum: function(status, labelp) {
+            var homeworkurl = status == "uncompleted" ? "https://www.shiguangkey.com/api/student/homework/getUnCompletedList" : "https://www.shiguangkey.com/api/student/homework/getCompletedList"
+            $.ajax({
+                type: "GET",
+                async: false,
+                cache: false,
+                headers: {
+                    "token": $.cookie.get("token"),
+                    "terminaltype": 4
+                },
+                cookie: document.cookie,
+                url: homeworkurl + "?classId=" + $.getUrlParam("classId") + "&activeKey=1&pageIndex=1&pageSize=10&terminalType=4",
+                dataType: "json",
+                success: function (res) {
+                    if (status == "uncompleted") {
+                        var uncompleted = $("<span style='color: red;' class='shiguangkeyHelper'>（" + res.data.totalItem + "）</span>");
+                        labelp.find("span[style='color: red;']").remove();
+                        labelp.append(uncompleted);
+                    } else {
+                        var completed = $("<span style='color: green;' class='shiguangkeyHelper'>（" + res.data.totalItem + "）</span>");
+                        labelp.find("span[style='color: green;']").remove();
+                        labelp.append(completed);
+                    }
+                },
+                error: function () {}
+            });
+        },
+        lastlearnselect: function() {
+            var lastlearnselectEle = $("a[onclick='shiguangkeyHelper.lastlearnselect()']").parent("li");
+            if (lastlearnselectEle.hasClass("_2Aysu")) {
+                lastlearnselectEle.removeClass("_2Aysu");
+                // 最近在学按钮再次点击，恢复课程展示，待实现。
+            } else {
+                $.ajax({
+                    type: "POST",
+                    async: false,
+                    cache: false,
+                    headers: {"token": $.cookie.get("token")},
+                    cookie: document.cookie,
+                    url: "https://www.shiguangkey.com/api/course/queryByStudent",
+                    data: {"pageIndex": 1, "pageSize":10, "courseType": 5310, "terminalType":4},
+                    dataType: "json",
+                    success: function (res) {
+                        var courses = res.data.courses;
+                        for (var i=0; i<courses.length; i++) {
+                            var course = courses[i];
+                            if (course.liveChapterStartTime || course.startTimeMills || course.startTime) {
+                                var courseHtml = '<div class="TlhQy"><a href="/course/{id}" target="_blank" rel="noopener noreferrer" class="_1TOYf"><img src="https://res.shiguangkey.com/{cover}?x-oss-process=image/format,webp" alt="封面"><span class="_27EfR _2VfA1">VIP</span></a><div class="_3f44N"><a href="/course/{id}" target="_blank" rel="noopener noreferrer" class="_1gBLk">{title}</a><div><div class="_25oe4">开课时间：<b>{t}</b></div><div class="_25oe4">所在班期：<b>{period}期</b></div></div><div class="_19ULe"><div class="_2_52z" href="javascript:;">联系老师<ul><div class="_1Zcg9"><div class="_2u4ro"><li><a href="tencent://message/?uin=3003302254&amp;Site=qq&amp;Menu=yes" target="_blank" rel="noopener noreferrer">在线查考勤木子</a></li><li><a href="tencent://message/?uin=2917322537&amp;Site=qq&amp;Menu=yes" target="_blank" rel="noopener noreferrer">05179-Tuple</a></li><li><a href="tencent://message/?uin=3002727582&amp;Site=qq&amp;Menu=yes" target="_blank" rel="noopener noreferrer">塔卡-05028</a></li><li><a href="tencent://message/?uin=&amp;Site=qq&amp;Menu=yes" target="_blank" rel="noopener noreferrer">Sky老师</a></li><li><a href="tencent://message/?uin=2446867994&amp;Site=qq&amp;Menu=yes" target="_blank" rel="noopener noreferrer">心蓝06258</a></li><li><a href="tencent://message/?uin=3002751317&amp;Site=qq&amp;Menu=yes" target="_blank" rel="noopener noreferrer">缇子05015</a></li><li><a href="tencent://message/?uin=3003806927&amp;Site=qq&amp;Menu=yes" target="_blank" rel="noopener noreferrer">忘仙_03179</a></li><li><a href="tencent://message/?uin=3002795033&amp;Site=qq&amp;Menu=yes" target="_blank" rel="noopener noreferrer">木子</a></li><li><a href="tencent://message/?uin=&amp;Site=qq&amp;Menu=yes" target="_blank" rel="noopener noreferrer">小拓</a></li><li><a href="tencent://message/?uin=3003302254&amp;Site=qq&amp;Menu=yes" target="_blank" rel="noopener noreferrer">南小帝</a></li><li><a href="tencent://message/?uin=&amp;Site=qq&amp;Menu=yes" target="_blank" rel="noopener noreferrer">Youkou</a></li><li><a href="tencent://message/?uin=879735038&amp;Site=qq&amp;Menu=yes" target="_blank" rel="noopener noreferrer">蓝羽</a></li></div></div></ul></div></div></div><div class="zBr3m"><span class="_3dqyA" onclick="shiguangkeyHelper.setclassreminder({id}, {classId})"><span class="WM317"></span></span><span>开课提醒已关闭</span></div><div style="display: flex; flex-direction: column; float: right;"><button class="_3xDIx" onclick="(function() {location.href = \'https://www.shiguangkey.com/live/{id}/{classId}\'})();">开始学习</button></div></div>'
+                                course.t = $.timeFormat((new Date(course.liveChapterStartTime)), "yyyy-MM-dd hh:mm")
+                                courseHtml = $.stringFormat(courseHtml, course)
+                                var courses_div = $("div[class='_2GxAN']").find("div[class='_2u4ro']");
+                                $(courses_div.find("div[class='TlhQy']")).remove();
+                                courses_div.append($(courseHtml));
+                            }
+                            $("div[class='_3vI_T']").remove();
+                        };
+                        lastlearnselectEle.addClass("_2Aysu");
+                    },
+                    error: function () {}
+                })
+            }
+        },
+        setclassreminder: function(courseId, classId) {
+            var classreminderEle = $($.stringFormat("span[onclick='shiguangkeyHelper.setclassreminder({courseId}, {classId})']", {courseId: courseId, classId: classId}));
+            if (classreminderEle.hasClass("p3Y8Q")) {
+                $.ajax({
+                    type: "POST",
+                    async: false,
+                    cache: false,
+                    headers: {
+                        "token": $.cookie.get("token")
+                    },
+                    cookie: document.cookie,
+                    url: "https://www.shiguangkey.com/api/interaction/cancelCourseRemond",
+                    data: {
+                        "courseId": courseId,
+                        "classId": classId,
+                        "terminalType": 4
+                    },
+                    dataType: "json",
+                    success: function (res) {
+                        if (res.status == 0) {
+                            classreminderEle.removeClass("p3Y8Q");
+                        }
+                    },
+                    error: function () {}
+                })
+            } else {
+                $.ajax({
+                    type: "POST",
+                    async: false,
+                    cache: false,
+                    headers: {
+                        "token": $.cookie.get("token")
+                    },
+                    cookie: document.cookie,
+                    url: "https://www.shiguangkey.com/api/interaction/setCourseRemond",
+                    data: {
+                        "courseId": courseId,
+                        "classId": classId,
+                        "terminalType": 4
+                    },
+                    dataType: "json",
+                    success: function (res) {
+                        if (res.status == 0) {
+                            classreminderEle.addClass("p3Y8Q");
+                        }
+                    },
+                    error: function () {}
+                })
+            }
+        },
+
+
+    }
 
     $.extend({
         stringFormat: function (str, obj) {
@@ -211,7 +261,7 @@
         },
         getUrlParam: function (name) {
             /*
-            获取当前页面URL中指定参数的值
+            获取当前页面URL路径里指定参数的值
             调用方式：$.getUrlParam("name")
             */
             var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
